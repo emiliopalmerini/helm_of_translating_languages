5b5c81f6c6c204175ae030cff28373ee05719931
emiliopalmerini <emilio.palmerini@gmail.com>
2023-03-12 11:29:10 +0100

    tdd - to_message throws when empty

--- flaskr/chat_message.py
+++ flaskr/chat_message.py
@@ -4,10 +4,28 @@ class ChatMessage:
         self.content = c
 
     def to_message(self):
-        if self.role is None or self.content is None:
-            raise ValueError("role and content cannot be None")
-        else:
+        if self.role and self.content:
             return (
                 """ "role": "{role}", "content": "{content}" """.format(
                     role=self.role, content=self.content
                 ))
+        else:
+            raise ValueError("role and content cannot be empty or None")
+
+
+class UserMessage(ChatMessage):
+    def __init__(self, c):
+        self.role = "user"
+        self.content = c
+
+
+class SystemMessage(ChatMessage):
+    def __init__(self, c):
+        self.role = "system"
+        self.content = c
+
+
+class BehaviourMessage(ChatMessage):
+    def __init__(self, c):
+        self.role = "assistant"
+        self.content = c
--- tests/test_chat_message.py
+++ tests/test_chat_message.py
@@ -1,17 +1,61 @@
 import pytest
-from flaskr.chat_message import ChatMessage
+from flaskr.chat_message import *
 
 
-
-def test_to_message():
-    obj = ChatMessage("test_role", "test_content")
+def test_ChatMessage_to_message():
+    obj = ChatMessage("test_role","test_content")
     assert obj.to_message() == """ "role": "test_role", "content": "test_content" """
 
 
 @pytest.mark.parametrize("message", [
+    ChatMessage(None, None),
     ChatMessage(None, "test_content"),
     ChatMessage("test_role", None),
+    ChatMessage(None, ""),
+    ChatMessage("", None),
+    ChatMessage("", "test_content"),
+    ChatMessage("test_role", ""),
+    ChatMessage("",""),
 ])
-def test_to_message_exception(message):
+def test_ChatMessage_to_message_exception(message):
+    with pytest.raises(ValueError):
+        message.to_message()
+
+def test_UserMessage_to_message():
+    obj = UserMessage("test_content")
+    assert obj.to_message() == """ "role": "user", "content": "test_content" """
+
+
+@pytest.mark.parametrize("message", [
+    UserMessage(None),
+    UserMessage(""),
+])
+def test_ChatMessage_to_message_exception(message):
+    with pytest.raises(ValueError):
+        message.to_message()
+
+def test_BehaviourMessage_to_message():
+    obj = BehaviourMessage("test_content")
+    assert obj.to_message() == """ "role": "assistant", "content": "test_content" """
+
+
+@pytest.mark.parametrize("message", [
+    BehaviourMessage(None),
+    BehaviourMessage(""),
+])
+def test_ChatMessage_to_message_exception(message):
+    with pytest.raises(ValueError):
+        message.to_message()
+
+def test_SystemMessage_to_message():
+    obj = SystemMessage("test_content")
+    assert obj.to_message() == """ "role": "system", "content": "test_content" """
+
+
+@pytest.mark.parametrize("message", [
+    SystemMessage(None),
+    SystemMessage(""),
+])
+def test_ChatMessage_to_message_exception(message):
     with pytest.raises(ValueError):
         message.to_message()

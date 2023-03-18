2c4194cd7cbf8cc74b0f68652675b260f8ad7e3f
emiliopalmerini <emilio.palmerini@gmail.com>
2023-03-18 17:45:19 +0100

    refactor(translator): break up generate_translation
    ## Problem
    generate_translation was difficult to test
    
    ## Solution
    Break up generate_translation in three different functions

--- flaskr/translator.py
+++ flaskr/translator.py
@@ -7,31 +7,37 @@ from openai import ChatCompletion
 #         self.model = kwargs.get('model')
 #         self.prompt = kwargs.get('prompt')
 #         self.set_temperature(kwargs.get('temperature'))
-    
+
 #     def set_temperature(self, temperature):
 #         if temperature is not None or "":
-            # return float(temperature)
+# return float(temperature)
 
 
 class OpenAiTranslator():
-    def __init__(self, text, languages = ['en']):
+    def __init__(self, text, languages=['en']):
         self.text = text
         self.languages = languages
-        
 
-    def generate_translation():
-        system = SystemMessage(
+    def generate_system_message(self):
+        return SystemMessage(
             """You are a super skilled translator who can translate any text to {language}.""".format(
                 language=self.languages),
         )
 
-        behaviour = BehaviourMessage(
+    def generate_behaviour_message(self):
+        return BehaviourMessage(
             """Shure, I can translate anything to {language}.""".format(language=self.languages))
 
-        translation = UserMessage(
+    def generate_translated_text(self):
+        return UserMessage(
             """Translate this "{text}" to {language}""".format(
                 text=self.text, language=self.language)
         )
+
+    def generate_translation(self):
+        system = self.generate_system_message()
+        behaviour = self.generate_behaviour_message()
+        translation = self.generate_translated_text()
         return ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[

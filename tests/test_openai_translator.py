d321d0498c84aeb07eb39cfedf9894d3a7f65442
emiliopalmerini <emilio.palmerini@gmail.com>
2023-03-18 17:43:38 +0100

    test(translator): add tests for translator file
    ## Problem
    I was tired and didn't used tdd for the translator
    
    ## Solution
    This adds tests. Next commit refactoring

--- tests/test_openai_translator.py
+++ tests/test_openai_translator.py
@@ -1,1 +1,28 @@
-# TODO implement test for translator
+import pytest
+from flaskr.translator import OpenAiTranslator
+
+
+@pytest.fixture
+def generate_translator():
+    return OpenAiTranslator('Ciao Mondo', 'English, Franch, Chinese')
+
+
+def test_generate_system_message():
+    translator = generate_translator()
+    translator.generate_system_message()
+
+    assert translator.system_message == 'You are a super skilled translator who can translate any text to English, French, Chinese.'
+
+
+def test_generate_behaviour_message():
+    translator = generate_translator()
+    translator.generate_behaviour_message()
+
+    assert translator.behaviour_message == 'Shure, I can translate anything to English, French, Chinese.'
+
+
+def test_generate_translated_message():
+    translator = generate_translator()
+    translator.generate_translated_text()
+
+    assert translator.translated_message == 'Translate this "Ciao Mondo" to English, French, Chinese.'

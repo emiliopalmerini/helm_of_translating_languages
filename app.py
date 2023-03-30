722ce4ffa02e6d204ef61e11ead2b9fd0f84cfaf
emiliopalmerini <emilio.palmerini@gmail.com>
2023-03-30 17:12:07 +0200

    feat: add notebook to menage file quickly
    TODO give the main program the same function

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from translator import OpenAiTranslator
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-app.secret_key = "supersecretkey"
+app.secret_key = 'supersecretkey'
 
 
 @app.route("/", methods=("GET", "POST"))
